00ae607288224e93410a8eef57b53e60a066746c
Memsbdm <mehmetbdm@outlook.fr>
2024-08-10 13:43:15 +0200

    refactor: change user id to uid

--- database/migrations/1723287790112_create_users_table.ts
+++ database/migrations/1723287790112_create_users_table.ts
@@ -5,7 +5,7 @@ export default class extends BaseSchema {
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id').notNullable()
+      table.uuid('id').primary().defaultTo(this.db.rawQuery('gen_random_uuid()').knexQuery)
       table.timestamp('created_at').notNullable()
       table.timestamp('updated_at').nullable()
       table.string('email', 254).notNullable().unique()
