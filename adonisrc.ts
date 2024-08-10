62bc732f6cb05844b3b086194324047a7c6ae2cc
Memsbdm <mehmetbdm@outlook.fr>
2024-08-10 20:47:02 +0200

    feat: setup mailer

--- adonisrc.ts
+++ adonisrc.ts
@@ -14,6 +14,7 @@ export default defineConfig({
     () => import('@adonisjs/core/commands'),
     () => import('@adonisjs/lucid/commands'),
     () => import('@tuyau/core/commands'),
+    () => import('@adonisjs/mail/commands'),
   ],
 
   /*
@@ -43,6 +44,7 @@ export default defineConfig({
     () => import('@adonisjs/auth/auth_provider'),
     () => import('@adonisjs/inertia/inertia_provider'),
     () => import('@tuyau/core/tuyau_provider'),
+    () => import('@adonisjs/mail/mail_provider'),
   ],
 
   /*
