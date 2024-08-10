d5624d942dfec7a52572bc2d686bbfbe02b99b9b
Memsbdm <mehmetbdm@outlook.fr>
2024-08-10 13:41:25 +0200

    chore: setup user

--- app/users/models/user.ts
+++ app/users/models/user.ts
@@ -1,30 +1,45 @@
-import { DateTime } from 'luxon'
 import hash from '@adonisjs/core/services/hash'
 import { compose } from '@adonisjs/core/helpers'
 import { BaseModel, column } from '@adonisjs/lucid/orm'
 import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
+import { Opaque } from '@adonisjs/core/types/helpers'
+import { DateTime } from 'luxon'
 
 const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
   uids: ['email'],
   passwordColumnName: 'password',
 })
 
+export type UserId = Opaque<'UserId', string>
+
 export default class User extends compose(BaseModel, AuthFinder) {
   @column({ isPrimary: true })
-  declare id: number
-
-  @column()
-  declare fullName: string | null
-
-  @column()
-  declare email: string
-
-  @column({ serializeAs: null })
-  declare password: string
+  declare id: UserId
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime | null
+
+  @column()
+  declare email: string
+
+  @column()
+  declare username: string
+
+  @column()
+  declare displayedName: string
+
+  @column()
+  declare firstName: string | null
+
+  @column()
+  declare lastName: string | null
+
+  @column()
+  declare phone: string | null
+
+  @column({ serializeAs: null })
+  declare password: string
 }
